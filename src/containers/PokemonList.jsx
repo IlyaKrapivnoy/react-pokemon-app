2c4da126627baa15467ad673e817a33c4af7d5d6
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-19 20:06:58 +0300

    useEffect

--- src/containers/PokemonList.jsx
+++ src/containers/PokemonList.jsx
@@ -1,9 +1,18 @@
 import { useDispatch, useSelector } from 'react-redux';
 import _ from 'lodash';
+import { useEffect } from 'react';
+import { GetPokemonList } from '../actions/pokemonActions';
 
 const PokemonList = () => {
     const dispatch = useDispatch();
     const pokemonList = useSelector((state) => state.PokemonList);
+    useEffect(() => {
+        FetchData(1);
+    });
+
+    const FetchData = (page = 1) => {
+        dispatch(GetPokemonList(page));
+    };
 
     const ShowData = () => {
         if (!_.isEmpty(pokemonList.data)) {
