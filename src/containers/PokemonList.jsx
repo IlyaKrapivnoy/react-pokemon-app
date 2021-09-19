1faddfbb574e95e00e833008302be39988c6a72b
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-19 21:57:04 +0300

    add search

--- src/containers/PokemonList.jsx
+++ src/containers/PokemonList.jsx
@@ -1,10 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import _ from 'lodash';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { GetPokemonList } from '../actions/pokemonActions';
 import { Link } from 'react-router-dom';
 
-const PokemonList = () => {
+const PokemonList = (props) => {
+    const [search, setSearch] = useState('');
+
     const dispatch = useDispatch();
     const pokemonList = useSelector((state) => state.PokemonList);
     useEffect(() => {
@@ -42,7 +44,23 @@ const PokemonList = () => {
         return <p>unable to get data</p>;
     };
 
-    return <div>{ShowData()}</div>;
+    return (
+        <div>
+            <div className='search-wrapper'>
+                <p>Search</p>
+                <input
+                    type='text'
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+                <button
+                    onClick={() => props.history.push(`/pokemon/${search}`)}
+                >
+                    Search
+                </button>
+            </div>
+            {ShowData()}
+        </div>
+    );
 };
 
 export default PokemonList;
