f6f45b7b832127672d643caa4c543f18fff2d483
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-19 20:03:13 +0300

    add if statements

--- src/containers/PokemonList.jsx
+++ src/containers/PokemonList.jsx
@@ -1,5 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
+import _ from 'lodash';
+
 const PokemonList = () => {
-    return <div>Pokemon List</div>;
+    const dispatch = useDispatch();
+    const pokemonList = useSelector((state) => state.PokemonList);
+
+    const ShowData = () => {
+        if (!_.isEmpty(pokemonList.data)) {
+            return <div>have data</div>;
+        }
+
+        if (pokemonList.loading) {
+            return <p>Loading...</p>;
+        }
+
+        if (pokemonList.errorMsg !== '') {
+            return <p>{pokemonList.errorMsg}</p>;
+        }
+
+        return <p>unable to get data</p>;
+    };
+
+    return <div>{ShowData()}</div>;
 };
 
 export default PokemonList;
