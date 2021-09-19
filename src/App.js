a44ce9b4a93699d75e04e594220ef413f2d6d38f
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-09-19 22:26:35 +0300

    change search btn path

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     return (
         <div className='App'>
             <nav>
-                <NavLink to={'/pokemon/test'}>Search</NavLink>
+                <NavLink to={'/'}>Search</NavLink>
             </nav>
             <Switch>
                 <Route path={'/'} exact component={PokemonList} />
